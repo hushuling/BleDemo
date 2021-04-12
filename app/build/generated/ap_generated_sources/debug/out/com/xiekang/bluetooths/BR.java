05423e2004787b26e31adbca57490684859f3fac
hushuling <huyunliang.huyunliang@outlook.com>
2021-04-12 13:51:58 +0800

    忽略提交

--- app/build/generated/ap_generated_sources/debug/out/com/xiekang/bluetooths/BR.java
+++
@@ -1,5 +0,0 @@
-package com.xiekang.bluetooths;
-
-public class BR {
-  public static final int _all = 0;
-}
--- app/build/generated/source/dataBinding/trigger/debug/com/xiekang/bluetooths/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.xiekang.bluetooths;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
